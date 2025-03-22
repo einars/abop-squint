f5a3830e2942cfc02e128162b4aee74791634127
Einārs Lielmanis <einars@spicausis.lv>
2025-03-22 13:45:20 +0200

    WIP

--- hellow/public/hellow.js
+++ hellow/public/hellow.js
@@ -1,10 +1,28 @@
 import * as squint_core from 'squint-cljs/core.js';
 var regl = createREGL(".gl-canvas");
+var Tau = (Math.PI) * (2);
+var angle_90 = (Tau) / (4);
+var angle_60 = (Tau) / (6);
+var scale_down = function (line) {
+const min_x1 = squint_core.apply(squint_core.min, squint_core.map(squint_core.first, line));
+const max_x2 = squint_core.apply(squint_core.max, squint_core.map(squint_core.first, line));
+const min_y3 = squint_core.apply(squint_core.min, squint_core.map(squint_core.second, line));
+const max_y4 = squint_core.apply(squint_core.max, squint_core.map(squint_core.second, line));
+const scale5 = squint_core.max((max_x2) - (min_x1), (max_y4) - (min_y3));
+const scale6 = (scale5) / (2);
+return squint_core.mapv((function (p__1047) {
+const vec__710 = p__1047;
+const x11 = squint_core.nth(vec__710, 0, null);
+const y12 = squint_core.nth(vec__710, 1, null);
+return squint_core.list((0.95) * ((((x11) - (min_x1)) / (scale6)) - (1)), (0.95) * ((((y12) - (min_y3)) / (scale6)) - (1)));;
+}), line);;
+};
 var square_island = ({ "me": ["F", "-", "F", "-", "F", "-", "F"], "F": ["F", "-", "F", "+", "F", "+", "F", "F", "-", "F", "-", "F", "+", "F"] });
 var bobo = ({ "me": ["F", "-", "F", "-", "F", "-", "F"], "F": "FF-F-F-F-F-F+F" });
 var fig1_9b = ({ "me": ["F", "-", "F", "-", "F", "-", "F"], "F": "FF-F-F-F-FF" });
 var fig1_9c = ({ "me": ["F", "-", "F", "-", "F", "-", "F"], "F": "FF-F+F-F-FF" });
-var fig1_10a = ({ "me": ["F1"], "F1": ["F1", "+", "FR", "+"], "FR": ["-", "F1", "-", "FR"] });
+var fig1_10a = ({ "name": "fig1-10a", "me": ["F1"], "F1": ["F1", "+", "FR", "+"], "FR": ["-", "F1", "-", "FR"] });
+var fig1_10b = ({ "name": "fig1-10b", "angle": angle_60, "me": ["F1"], "F1": ["FR", "+", "F1", "+", "FR"], "FR": ["F1", "-", "FR", "-", "F1"] });
 var koch_step = function (koch) {
 let current1 = squint_core.get(koch, "me");
 let accum2 = [];
@@ -13,9 +31,9 @@ if (squint_core.not(current1)) {
 return squint_core.assoc(koch, "me", accum2)} else {
 const temp__23717__auto__3 = squint_core.get(koch, squint_core.first(current1));
 if (squint_core.truth_(temp__23717__auto__3)) {
-const transf4 = temp__23717__auto__3;
+const replacement4 = temp__23717__auto__3;
 let G__5 = squint_core.next(current1);
-let G__6 = squint_core.apply(squint_core.conj, accum2, transf4);
+let G__6 = squint_core.into(accum2, replacement4);
 current1 = G__5;
 accum2 = G__6;
 continue;
@@ -29,115 +47,90 @@ continue;
 }
 ;
 };
-var move = function (p__709, facing) {
-const vec__14 = p__709;
+var move = function (p__1048, angle) {
+const vec__14 = p__1048;
 const x5 = squint_core.nth(vec__14, 0, null);
 const y6 = squint_core.nth(vec__14, 1, null);
-const pred__7107 = squint_core._EQ_;
-const expr__7118 = facing;
-if (squint_core.truth_(pred__7107("up", expr__7118))) {
-return [x5, (y6 + 1)]} else {
-if (squint_core.truth_(pred__7107("dn", expr__7118))) {
-return [x5, (y6 - 1)]} else {
-if (squint_core.truth_(pred__7107("rt", expr__7118))) {
-return [(x5 + 1), y6]} else {
-if (squint_core.truth_(pred__7107("lt", expr__7118))) {
-return [(x5 - 1), y6]} else {
-throw new IllegalArgumentException(squint_core.str("No matching clause: ", expr__7118))}}}};;;
+return [(x5) + (Math.cos(angle)), (y6) + (Math.sin(angle))];;
 };
-var rot_left = ({ "up": "lt", "lt": "dn", "dn": "rt", "rt": "up" });
-var rot_right = ({ "up": "rt", "rt": "dn", "dn": "lt", "lt": "up" });
-var scale_down = function (line) {
-const min_x1 = squint_core.apply(squint_core.min, squint_core.map(squint_core.first, line));
-const max_x2 = squint_core.apply(squint_core.max, squint_core.map(squint_core.first, line));
-const min_y3 = squint_core.apply(squint_core.min, squint_core.map(squint_core.second, line));
-const max_y4 = squint_core.apply(squint_core.max, squint_core.map(squint_core.second, line));
-const scale5 = squint_core.max((max_x2) - (min_x1), (max_y4) - (min_y3));
-const scale6 = (scale5) / (2);
-squint_core.println(squint_core.mapv((function (p__712) {
-const vec__710 = p__712;
-const x11 = squint_core.nth(vec__710, 0, null);
-const y12 = squint_core.nth(vec__710, 1, null);
-return squint_core.list((x11) / (scale6), (y12) / (scale6));;
-}), line));
-return squint_core.mapv((function (p__713) {
-const vec__1316 = p__713;
-const x17 = squint_core.nth(vec__1316, 0, null);
-const y18 = squint_core.nth(vec__1316, 1, null);
-return squint_core.list((0.95) * ((((x17) - (min_x1)) / (scale6)) - (1)), (0.95) * ((((y18) - (min_y3)) / (scale6)) - (1)));;
-}), line);;
-};
-var materialize = function (koch) {
+var materialize = function (koch, turn_angle) {
 squint_core.println("materializing", squint_core.count(squint_core.get(koch, "me")));
 let koch1 = squint_core.get(koch, "me");
 let pos2 = [0, 0];
-let facing3 = "lt";
+let facing3 = 0;
 let accum4 = [[0, 0]];
 while(true){
-const pred__7145 = squint_core._EQ_;
-const expr__7156 = squint_core.first(koch1);
-if (squint_core.truth_(pred__7145("F", expr__7156))) {
-let G__7 = squint_core.next(koch1);
-let G__8 = move(pos2, facing3);
-let G__9 = facing3;
-let G__10 = squint_core.conj(accum4, move(pos2, facing3));
-koch1 = G__7;
-pos2 = G__8;
-facing3 = G__9;
-accum4 = G__10;
+const npos5 = move(pos2, facing3);
+const nkoch6 = squint_core.next(koch1);
+const G__10497 = squint_core.first(koch1);
+switch (G__10497) {case "F":
+let G__9 = nkoch6;
+let G__10 = npos5;
+let G__11 = facing3;
+let G__12 = squint_core.conj(accum4, npos5);
+koch1 = G__9;
+pos2 = G__10;
+facing3 = G__11;
+accum4 = G__12;
 continue;
-} else {
-if (squint_core.truth_(pred__7145("F1", expr__7156))) {
-let G__11 = squint_core.next(koch1);
-let G__12 = move(pos2, facing3);
-let G__13 = facing3;
-let G__14 = squint_core.conj(accum4, move(pos2, facing3));
-koch1 = G__11;
-pos2 = G__12;
-facing3 = G__13;
-accum4 = G__14;
+
+break;
+case "F1":
+let G__13 = nkoch6;
+let G__14 = npos5;
+let G__15 = facing3;
+let G__16 = squint_core.conj(accum4, npos5);
+koch1 = G__13;
+pos2 = G__14;
+facing3 = G__15;
+accum4 = G__16;
 continue;
-} else {
-if (squint_core.truth_(pred__7145("FR", expr__7156))) {
-let G__15 = squint_core.next(koch1);
-let G__16 = move(pos2, facing3);
-let G__17 = facing3;
-let G__18 = squint_core.conj(accum4, move(pos2, facing3));
-koch1 = G__15;
-pos2 = G__16;
-facing3 = G__17;
-accum4 = G__18;
+
+break;
+case "FR":
+let G__17 = nkoch6;
+let G__18 = npos5;
+let G__19 = facing3;
+let G__20 = squint_core.conj(accum4, npos5);
+koch1 = G__17;
+pos2 = G__18;
+facing3 = G__19;
+accum4 = G__20;
 continue;
-} else {
-if (squint_core.truth_(pred__7145("+", expr__7156))) {
-let G__19 = squint_core.next(koch1);
-let G__20 = pos2;
-let G__21 = squint_core.get(rot_right, facing3);
-let G__22 = accum4;
-koch1 = G__19;
-pos2 = G__20;
-facing3 = G__21;
-accum4 = G__22;
+
+break;
+case "+":
+let G__21 = nkoch6;
+let G__22 = pos2;
+let G__23 = (facing3) + (turn_angle);
+let G__24 = accum4;
+koch1 = G__21;
+pos2 = G__22;
+facing3 = G__23;
+accum4 = G__24;
 continue;
-} else {
-if (squint_core.truth_(pred__7145("-", expr__7156))) {
-let G__23 = squint_core.next(koch1);
-let G__24 = pos2;
-let G__25 = squint_core.get(rot_left, facing3);
-let G__26 = accum4;
-koch1 = G__23;
-pos2 = G__24;
-facing3 = G__25;
-accum4 = G__26;
+
+break;
+case "-":
+let G__25 = nkoch6;
+let G__26 = pos2;
+let G__27 = (facing3) - (turn_angle);
+let G__28 = accum4;
+koch1 = G__25;
+pos2 = G__26;
+facing3 = G__27;
+accum4 = G__28;
 continue;
-} else {
-return scale_down(accum4)}}}}};;;break;
+
+break;
+default:
+return scale_down(accum4)};;;;break;
 }
 ;
 };
 var frag = "\nprecision mediump float;\nuniform vec4 color;\nvoid main() {\n  gl_FragColor = color;\n}\n  ";
 var vert = "\nprecision mediump float;\nattribute vec2 position;\nvoid main() {\n  gl_Position = vec4(position, 0, 1);\n}\n  ";
-var slow_single_line = regl(({ "frag": frag, "vert": vert, "uniforms": ({ "color": [1, 0, 0, 1] }), "attributes": ({ "position": regl.prop("points") }), "elements": (function (ctx, props, batchid) {
+var continuous_line = regl(({ "frag": frag, "vert": vert, "uniforms": ({ "color": [1, 0, 0, 1] }), "attributes": ({ "position": regl.prop("points") }), "elements": (function (ctx, props, batchid) {
 return squint_core.mapv((function (_PERCENT_1) {
 return squint_core.list(_PERCENT_1, (_PERCENT_1 + 1));
 }), squint_core.range((squint_core.count(squint_core.get(props, "points")) - 1)));
@@ -148,9 +141,17 @@ return squint_core.nth(squint_core.iterate(koch_step, rules), generation);
 var draw_triangle = regl(({ "frag": frag, "vert": vert, "attributes": ({ "position": regl.buffer([[-2, -2], [4, -2], [4, 4]]) }), "uniforms": ({ "color": regl.prop("color") }), "count": 3 }));
 var main = function () {
 console.log("Booting up");
+const vec__14 = [fig1_10b, 7];
+const algo5 = squint_core.nth(vec__14, 0, null);
+const iterations6 = squint_core.nth(vec__14, 1, null);
+const k7 = materialize(get_koch(algo5, iterations6), squint_core.get(algo5, "angle", angle_90));
+regl.frame((function () {
 regl.clear(({ "color": [1, 1, 1, 1], "depth": 1 }));
-return slow_single_line(({ "points": materialize(get_koch(fig1_10a, 12)) }));
+return continuous_line(({ "points": k7 }));;
+}));
+return document.querySelector(".text").innerHTML = squint_core.str(squint_core.get(algo5, "name"), ", ", iterations6, " iterācijas, ", squint_core.count(k7), " segmenti");
+;;
 };
 main();
 
-export { frag, materialize, regl, square_island, fig1_10a, vert, get_koch, draw_triangle, rot_left, main, fig1_9b, bobo, move, rot_right, koch_step, fig1_9c, scale_down, slow_single_line }
+export { continuous_line, frag, materialize, regl, square_island, fig1_10a, angle_90, vert, get_koch, draw_triangle, angle_60, main, fig1_9b, fig1_10b, bobo, move, koch_step, fig1_9c, scale_down, Tau }
--- hellow/src/hellow.cljs
+++ hellow/src/hellow.cljs
@@ -1,6 +1,24 @@
-(ns hellow)
 
 (def regl (js/createREGL ".gl-canvas"))
+
+(def Tau (* js/Math.PI 2))
+(def angle-90 (/ Tau 4))
+(def angle-60 (/ Tau 6))
+
+
+(defn scale-down [line]
+  (let [min-x (apply min (map first line))
+        max-x (apply max (map first line))
+        min-y (apply min (map second line))
+        max-y (apply max (map second line))
+        scale (max 
+                (- max-x min-x) 
+                (- max-y min-y))
+        scale (/ scale 2)]
+    (mapv (fn [[x y]] (list 
+                        (* 0.95 (- (/ (- x min-x) scale) 1))
+                        (* 0.95 (- (/ (- y min-y) scale) 1)))) line)
+    ))
 
 
 (def square-island {:me [:F :- :F :- :F :- :F]
@@ -14,66 +32,45 @@
 (def fig1-9c {:me [:F :- :F :- :F :- :F]
               :F "FF-F+F-F-FF"})
 
-(def fig1-10a {:me [:F1]
+(def fig1-10a {:name "fig1-10a"
+               :me [:F1]
                :F1 [:F1 :+ :FR :+]
                :FR [:- :F1 :- :FR]})
+
+
+(def fig1-10b {:name "fig1-10b"
+               :angle angle-60
+               :me [:F1]
+               :F1 [:FR :+ :F1 :+ :FR]
+               :FR [:F1 :- :FR :- :F1]})
 
 (defn koch-step [koch]
   (loop [current (:me koch), accum []]
     (if-not current
       (assoc koch :me accum)
-      (if-let [transf (get koch (first current))]
-        (recur (next current) (apply conj accum transf))
+      (if-let [replacement (get koch (first current))]
+        (recur (next current) (into accum replacement))
         (recur (next current) (conj accum (first current)))))))
 
 
 
-(defn move [[x y] facing]
-  (condp = facing
-    :up [x (inc y)]
-    :dn [x (dec y)]
-    :rt [(inc x) y]
-    :lt [(dec x) y]))
-
-(def rot-left 
-  {:up :lt
-   :lt :dn
-   :dn :rt
-   :rt :up})
-
-(def rot-right
-  {:up :rt
-   :rt :dn
-   :dn :lt
-   :lt :up})
-
-(defn scale-down [line]
-  (let [min-x (apply min (map first line))
-        max-x (apply max (map first line))
-        min-y (apply min (map second line))
-        max-y (apply max (map second line))
-        scale (max 
-                (- max-x min-x) 
-                (- max-y min-y))
-        scale (/ scale 2)
-        ]
-    (println (mapv (fn [[x y]] (list (/ x scale) (/ y scale))) line))
-    (mapv (fn [[x y]] (list 
-                        (* 0.95 (- (/ (- x min-x) scale) 1))
-                        (* 0.95 (- (/ (- y min-y) scale) 1)))) line)
-    ))
+(defn move [[x y] angle]
+  [(+ x (js/Math.cos angle))
+   (+ y (js/Math.sin angle))])
 
 
-(defn materialize [koch]
+(defn materialize [koch turn-angle]
   (println "materializing" (count (:me koch)))
-  (loop [koch (:me koch), pos [0 0], facing :lt, accum [[0 0]]]
-    (condp = (first koch)
-      :F (recur (next koch) (move pos facing) facing (conj accum (move pos facing)))
-      :F1 (recur (next koch) (move pos facing) facing (conj accum (move pos facing)))
-      :FR (recur (next koch) (move pos facing) facing (conj accum (move pos facing)))
-      :+ (recur (next koch) pos (get rot-right facing) accum)
-      :- (recur (next koch) pos (get rot-left facing) accum)
-      (scale-down accum))))
+  (loop [koch (:me koch), pos [0 0], facing 0.0, accum [[0 0]]]
+    (let [npos (move pos facing)
+          nkoch (next koch)]
+      (case (first koch)
+        :F (recur nkoch npos facing (conj accum npos))
+        :F1 (recur nkoch npos facing (conj accum npos))
+        :FR (recur nkoch npos facing (conj accum npos))
+        :+ (recur nkoch pos (+ facing turn-angle) accum)
+        :- (recur nkoch pos (- facing turn-angle) accum)
+        (scale-down accum)))))
 
 
 (def frag "
@@ -92,7 +89,7 @@ void main() {
 }
   ")
 
-(def slow-single-line []
+(def continuous-line []
   (regl
     {:frag frag
      :vert vert
@@ -115,17 +112,21 @@ void main() {
      :uniforms {:color (regl/prop "color")}
      :count 3 }))
 
-
 (defn main[]
   (js/console.log "Booting up")
-  (regl/clear {:color [1, 1, 1, 1]
-               :depth 1 })
-  #_(slow-single-line {:points (materialize (get-koch square-island 3))})
-  #_(slow-single-line {:points (materialize (get-koch bobo 4))})
-  #_(slow-single-line {:points (materialize (get-koch fig1-9b 4))})
-  #_(slow-single-line {:points (materialize (get-koch fig1-9c 4))})
-  (slow-single-line {:points (materialize (get-koch fig1-10a 12))})
-
-  #_(regl/frame frame))
+  (let [;[algo iterations] [fig1-10a 13]
+        ;[algo iterations] [square-island 3]
+        ;[algo iterations] [bobo 4]
+        ;[algo iterations] [fig1-9b 4]
+        ;[algo iterations angle] [fig1-9c 4 angle-90]
+        ;[algo iterations angle] [fig1-9c 3 angle-90]
+        [algo iterations] [fig1-10b 7]
+        k (materialize (get-koch algo iterations) (:angle algo angle-90))]
+    (regl/frame #(do
+                   (regl/clear {:color [1, 1, 1, 1]
+                                :depth 1 })
+                   (continuous-line {:points k})))
+    (set! (.-innerHTML (js/document.querySelector ".text")) (str (:name algo) ", " iterations " iterācijas, " (count k) " segmenti"))
+    ))
 
 (main)
